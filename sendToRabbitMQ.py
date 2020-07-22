2c66494c9009aad8784c7595f16daef0fc6afd5c
junkwon <dydqja1013@naver.com>
2020-07-22 20:56:09 +0900

    Edit sendToRabbitMQ.py

--- sendToRabbitMQ.py
+++ sendToRabbitMQ.py
@@ -5,12 +5,12 @@ import loadUrls as lu
 credentials = pika.PlainCredentials('muna','muna112358!')
 connection = pika.BlockingConnection(pika.ConnectionParameters('13.124.107.195',5672,'/',credentials))
 channel = connection.channel()
-channel.queue_declare(queue='URLS')
+channel.queue_declare(queue='URL',durable=True, arguments={'x-dead-letter-exchange':'Crawler_URL_Exchange','x-dead-letter-routing-key':'URL_dead'})
 
 urls=lu.loadUrls()
 for url in urls:
     channel.basic_publish(exchange ='',
-                      routing_key = 'URLS',
+                      routing_key = 'URL',
                       body=url)
 print("Sending completed")
 
