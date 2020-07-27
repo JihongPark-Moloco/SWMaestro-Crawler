ecdd37e7dfbc8165d917fff37d843ef869743a84
junkwon <dydqja1013@naver.com>
2020-07-28 02:48:59 +0900

    Edit sendToRabbitMQ.py
    delete queue declaer

--- sendToRabbitMQ.py
+++ sendToRabbitMQ.py
@@ -6,8 +6,6 @@ import loadUrls as lu
 credentials = pika.PlainCredentials('muna', 'muna112358!')
 connection = pika.BlockingConnection(pika.ConnectionParameters('13.124.107.195', 5672, '/', credentials))
 channel = connection.channel()
-channel.queue_declare(queue='URL', durable=True, arguments={'x-dead-letter-exchange': 'Crawler_URL_Exchange',
-                                                            'x-dead-letter-routing-key': 'URL_dead'})
 
 urls = lu.loadUrls()
 for url in urls:
