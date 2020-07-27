85675be81c5112ca56fe60735050da113c8ff486
junkwon <dydqja1013@naver.com>
2020-07-28 02:55:16 +0900

    Edit loadUrls.py
    edit where need process = 0 -> where channel_name = null

--- loadUrls.py
+++ loadUrls.py
@@ -8,7 +8,7 @@ def loadUrls():
         conn = pg2.connect(database="createtrend", user="muna", password="muna112358!", host="13.124.107.195",
                            port="5432")
         cur = conn.cursor()
-        cur.execute("SELECT channel_url from channel where need_process = True")
+        cur.execute("SELECT channel_url from channel where channel_name is null")
         rows = cur.fetchall()
         newrows = [row[0] for row in rows]
         [print(row) for row in newrows]
