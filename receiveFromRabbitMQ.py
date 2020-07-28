935463dcbe12cc6dfdfef6fe39f98d13a7616e8e
junkwon <dydqja1013@naver.com>
2020-07-28 14:27:19 +0900

    Edit receiveFromRabbitMQ.py
    Enable auto_ack

--- receiveFromRabbitMQ.py
+++ receiveFromRabbitMQ.py
@@ -15,11 +15,13 @@ def callback(ch, method, properties, body):
     print(" [x] Received %r" % body.decode())
     if YouTube_Crawler.main(body.decode()):
         # 크롤러가 정상 종료 되었을때 ack를 보냅니다. 이때 메세지큐에서 해당 주소가 삭제됩니다.
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+        print('success')
     else:
         # 크롤러가 에러를 띄우면 negative ack를 보냅니다.
         # URL 큐는 큐의 x-dead-letter 설정에 따라 해당 메세지를 URL_dead 큐로 보냅니다.
-        ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
+        channel.basic_publish(exchange ='URL_exchange',
+                      routing_key = 'URL_dead',
+                      body=body.decode())
     # if YouTube_Crawler.main(body.decode()):
     #     return
     # else:
@@ -30,7 +32,8 @@ def callback(ch, method, properties, body):
 # auto_ack가 True일 경우 메세지를 꺼내오는 순간에 메세지 큐에서 해당 메세지는 삭제됩니다.
 # 만약 해당 주소를 크롤러가 받아와서 도는 도중에 크롤러가 중간에 에러를 띄우고
 # 프로세스가 중간에 죽어버릴 경우 정상적으로 처리가 안되었지만 메세지 큐에는 해당 주소가 없는 상황이 발생합니다.
-channel.basic_consume(queue='URL', on_message_callback=callback, auto_ack=False)
+channel.basic_consume(queue='URL', on_message_callback=callback)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
+
