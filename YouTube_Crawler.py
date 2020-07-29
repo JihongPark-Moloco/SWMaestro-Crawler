d84aca049dcd5fd9dce3c748a8734eeec1bef4c7
junkwon <dydqja1013@naver.com>
2020-07-29 15:09:17 +0900

    Edit receiveFromRabbitMQ.py

--- YouTube_Crawler.py
+++ YouTube_Crawler.py
@@ -54,9 +54,8 @@ def getDriver():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    # options.add_argument("disable-gpu")
-    driver = Chrome(executable_path=r"/home/ubuntu/Crawler/chromedriver",
-                    options=options)  # ,chrome_options=options
+    options.add_argument("disable-gpu")
+    driver = Chrome(executable_path="/Users/jun/juny/swm/crawler/muna-3/chromedriverjun")  # ,chrome_options=options
     driver.set_window_size(1920, 1080)
 
 
